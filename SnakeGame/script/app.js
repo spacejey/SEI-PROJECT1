4b09cd736c9edb754eabc8ca0a50ba8eb44840bf
Eunyeong Jeong <spaceyoung2@gmail.com>
2023-02-21 16:46:05 +0000

    project process2

--- SnakeGame/script/app.js
+++ SnakeGame/script/app.js
@@ -2,20 +2,23 @@ function init() {
 
   const grid = document.querySelector('.grid')
   const button = document.querySelector('button')
+  const score = document.querySelector('.score')
+
   
-  //엘레먼트 만들기
+  //elements
+  const snakeStart = [324, 323, 322]
+  let snake = snakeStart
 
-  const snakeStart = 234
-  let snakeCurrent = snakeStart
   let currentFood
-  let runSnake
-
-
-  // 그리드 만들기
+  // console.log(Array.isArray(snake))
+  
+  
+  //Grid Elements
   const width = 20
   const totalCell = width * width
   const cells = []
 
+  //! Creating Grid
   function createGrid() {
     for (let i = 0; i < totalCell; i++){
       const cell = document.createElement('div')
@@ -24,46 +27,70 @@ function init() {
       cells.push(cell)
     }
     addFood(currentFood)
-    addSnake(snakeStart)
+    addSnake()
     
   }
 
   
 
 
-  //게임 시작
+  //!게임 시작
   function startGame(){
     
-    //리셋펑션으로 초기화
-    reset() 
-    //뱀이 일정 속도로 움직인다.
-    // 뱀이 음식을 잡았을 때 펑션
-    snakeWin()
-    // if 뱀이 그리드 밖을 넘어갔을 때 펑션 -> gameOver()펑션
-    //음식 펑션
+    //reset
+    reset()
+    //snake's starting to move in grid
+
+    //if snake and food is on the same cell-function
+    snakeEat()
+
+    // if snake index is out of the width -> gameOver()펑션
+
+    //add food
     addFood()
-    
-  }
-
-  // 뱀이 음식을 잡았을 때
-  function snakeWin(){
-    if (snakeCurrent === currentFood){
-      //음식 색 바꾸기
-      console.log('catch')
-    } 
-    //랜덤음식 추가
-    //뱀 어레이 추가 push
-  }
-
-  //뱀 위치 추가
-  function addSnake(position){
-    cells[position].classList.add('snake')
-  }
-  function removeSnake(){
-    cells[snakeCurrent].classList.remove('snake')
   }
   
-  //뱀 움직이기
+  function reset() {
+    removeSnake()
+    removeFood()
+  }
+  
+
+  
+
+  //! when snake and food is on the same cell
+  function snakeEat(){
+
+    if (cells[snake[0]].classList.contains('food')) {
+      console.log('catch')
+      removeFood()
+      addFood()
+      // totalScore++
+      // scoreButton.innerText = totalScore
+      // score.innerHTML
+      
+      // add +1 in snake array 
+      // const snake = [0]
+      // snake.push([snake])
+      snake.push(snake[0])
+    }
+  }
+
+
+  //! add snake
+  function addSnake(){
+    snake.forEach(snakeCell => {
+      cells[snakeCell].classList.add('snake')
+    })
+  }
+  
+  function removeSnake(){
+    snake.forEach(snakeCell => {
+      cells[snakeCell].classList.remove('snake')
+    })
+  }
+  
+  //! snake direction
   function moveSnake(e){
     const right = 39
     const left = 37
@@ -72,28 +99,52 @@ function init() {
     
     removeSnake()
     
-    if (e.keyCode === right){
-      snakeCurrent++
+    if (e.keyCode === right && width !== 1){
+      snake.unshift(snake[0] + 1)
+      snake.pop()
+
     } else if (e.keyCode === left){
-      snakeCurrent--
+      snake.unshift(snake[0] - 1)
+      snake.pop()
+
     } else if (e.keyCode === up){
-      snakeCurrent -= width
+      // snake -= width
+      snake.unshift(snake[0] - 20)
+      snake.pop()
+
     } else if (e.keyCode === down){
-      snakeCurrent += width
+      // snake +=  width
+      snake.unshift(snake[0] + 20)
+      snake.pop()
+
     } else {
       console.log()
     }
-    console.log(snakeCurrent % width)
-    addSnake(snakeCurrent)
+    console.log(snake)
+
+    addSnake()
+    snakeEat()
   }
-  // setInterval(addSnake, 1000 / 10)
+
+
   
-  //랜덤 타겟 
+  //! add Food ( Random )
   function addFood(){
     const food = Math.floor(Math.random() * totalCell)
     currentFood = cells[food]
     currentFood.classList.add('food')
+    console.log(currentFood.dataset['index'])
   }
+  // function removeFood(){
+  //   cells.classList.remove('food')
+  // }
+  function removeFood() {
+    cells.forEach(cell => {
+      cell.classList.remove('food')
+    })
+  }
+
+
 
 
 
