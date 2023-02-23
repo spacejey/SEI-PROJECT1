29c866e8921b2318719ed32991d040521e8ba4dd
Eunyeong Jeong <spaceyoung2@gmail.com>
2023-02-23 19:01:26 +0000

    Project Process

--- SnakeGame/script/app.js
+++ SnakeGame/script/app.js
@@ -2,17 +2,16 @@ function init() {
 
   const grid = document.querySelector('.grid')
   const button = document.querySelector('button')
-  const score = document.querySelector('.score')
 
   
   //elements
-  const snakeStart = [324, 323, 322]
+  const snakeStart = [305, 306]
   let snake = snakeStart
-
   let currentFood
-  // console.log(Array.isArray(snake))
-  
-  
+  let score = 0
+  let activeSnake
+  let snakeDirection = 'right'
+
   //Grid Elements
   const width = 20
   const totalCell = width * width
@@ -28,26 +27,26 @@ function init() {
     }
     addFood(currentFood)
     addSnake()
-    
+  }
+  
+  //!게임 시작
+  function defaultBtn() {
+    button.innerText = 'Restart!'
+    reset()
+    addFood()
+    addSnake()
   }
 
+  function startGame(e){
+    button.innerText = `score! ${score}`
+    addSnake
+    snakeMovementTimer()
+  }
   
-
-
-  //!게임 시작
-  function startGame(){
-    
-    //reset
-    reset()
-    //snake's starting to move in grid
-
-    //if snake and food is on the same cell-function
-    snakeEat()
-
-    // if snake index is out of the width -> gameOver()펑션
-
-    //add food
-    addFood()
+  function gameOver(){
+    clearInterval(activeSnake)
+    alert('Game Over!')
+    defaultBtn()
   }
   
   function reset() {
@@ -55,9 +54,6 @@ function init() {
     removeFood()
   }
   
-
-  
-
   //! when snake and food is on the same cell
   function snakeEat(){
 
@@ -65,13 +61,8 @@ function init() {
       console.log('catch')
       removeFood()
       addFood()
-      // totalScore++
-      // scoreButton.innerText = totalScore
-      // score.innerHTML
-      
-      // add +1 in snake array 
-      // const snake = [0]
-      // snake.push([snake])
+      score += 10
+      button.innerText = `score! ${score}`
       snake.push(snake[0])
     }
   }
@@ -89,42 +80,66 @@ function init() {
       cells[snakeCell].classList.remove('snake')
     })
   }
-  
-  //! snake direction
-  function moveSnake(e){
+
+  function snakeMovementTimer() {
+    activeSnake = setInterval(() => {
+      removeSnake()
+      moveSnake()
+    }, 2000 / 10)
+
+  }
+
+  function changeSnakeDirection(e){
+
     const right = 39
     const left = 37
     const up = 38
     const down = 40
-    
-    removeSnake()
-    
-    if (e.keyCode === right && width !== 1){
-      snake.unshift(snake[0] + 1)
-      snake.pop()
 
-    } else if (e.keyCode === left){
-      snake.unshift(snake[0] - 1)
-      snake.pop()
+    if (e.keyCode === right) {
+      snakeDirection = 'right'
+    } else if (e.keyCode === left) {
+      snakeDirection = 'left'
+    } else if (e.keyCode === up) {
+      snakeDirection = 'up'
+    } else if (e.keyCode === down) {
+      snakeDirection = 'down'
+    }
+  }
+  
 
-    } else if (e.keyCode === up){
-      // snake -= width
-      snake.unshift(snake[0] - 20)
-      snake.pop()
-
-    } else if (e.keyCode === down){
-      // snake +=  width
-      snake.unshift(snake[0] + 20)
-      snake.pop()
-
+  function moveSnake(){
+      
+    if (snakeDirection === 'right' && snake[0] % width === width - 1){
+      gameOver()
+    } else if (snakeDirection === 'left' && snake[0] % width === 0){
+      gameOver()
+    } else if (snakeDirection === 'up' && snake[0] < width){
+      gameOver()
+    } else if (snakeDirection === 'down' && snake[0] + width >= totalCell){
+      gameOver()
     } else {
-      console.log()
+      removeSnake()
+      snake.pop()
+      if (snakeDirection === 'right'){
+        console.log('snake', snake[0] + 1)
+        snake.unshift(snake[0] + 1)
+      } else if (snakeDirection === 'left'){
+        snake.unshift(snake[0] - 1)
+      } else if (snakeDirection === 'up'){
+        snake.unshift(snake[0] - width)
+      } else if (snakeDirection === 'down'){
+        snake.unshift(snake[0] + width)
+      } 
+      if (cells[snake[0]].classList.contains('snake')) {
+        gameOver()
+      }
+      addSnake()
     }
     console.log(snake)
-
-    addSnake()
     snakeEat()
   }
+
 
 
   
@@ -135,9 +150,7 @@ function init() {
     currentFood.classList.add('food')
     console.log(currentFood.dataset['index'])
   }
-  // function removeFood(){
-  //   cells.classList.remove('food')
-  // }
+
   function removeFood() {
     cells.forEach(cell => {
       cell.classList.remove('food')
@@ -150,7 +163,7 @@ function init() {
 
 
 
-  document.addEventListener('keydown', moveSnake)
+  document.addEventListener('keydown', changeSnakeDirection)
   button.addEventListener('click', startGame)
 
   createGrid()
